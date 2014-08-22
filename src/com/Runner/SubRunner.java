ac0799acb0d1f13211a7ae581054b3f54c21614d
admin <admin@agagoms>
2014-08-22 18:01:14 +0900

    Info:

---
+++ src/com/Runner/SubRunner.java
@@ -0,0 +1,31 @@
+package com.Runner;
+
+public class SubRunner {
+
+	
+
+
+
+/*
+List<String> cmd = new ArrayList<String>();
+
+cmd.add("java");
+
+cmd.add("-classpath");
+
+cmd.add("\"d:/workspace/some project/lib/something.jar\"");
+
+cmd.add("blah.blah.SomeClass");
+
+cmd.add("arg1");
+
+cmd.add("arg2");
+
+ProcessBuilder bld = new ProcessBuilder(cmd);
+
+bld.directory(new File("d:/workspace/some project"));
+
+bld.start();
+*/	
+	
+}
